c5426154c8553fa1eaf06f0acebf519228a0b2e2
Rupa <rupa211984@gmail.com>
2019-12-11 17:24:21 -0800

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,37 @@
+class GenresController < ApplicationController
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    genre = Genre.new(genre_params)
+
+    if genre.save
+      redirect_to genre
+    end
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    genre = Genre.find(params[:id])
+
+    if genre.update(genre_params)
+      redirect_to genre
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
+end
